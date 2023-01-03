fc88964ec9cfaa8d623d8dfe3d326f4f5781e1ca
Lum Jahaj <lumjahaj@live.com>
2023-01-03 11:49:42 +0100

    initial commit

---
+++ src/test/java/com/elba/proficiencytest/ProficiencyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elba.proficiencytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProficiencyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
